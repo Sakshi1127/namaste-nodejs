4af9956296bb8e469746ee5a83153cbe299f2414
Sakshi <sakshimaheshwari19191@gmail.com>
2025-03-14 00:39:08 +0530

    route handle 2

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const app =express()
 
 //route handlers
 
+// app.use("route",[rh1,rh2,rh3,rh4])   we call write like this
+
+// app.use("route",[rh1,rh2],rh3,rh4)
+
+
 app.use('/user',(req,res,next)=>{
     console.log("Route handler 1")
     // res.send('Route handler 1')
