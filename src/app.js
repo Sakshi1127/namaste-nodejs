05189e3ac8afd8264ddc10192c618979a2e63a7d
Sakshi <sakshimaheshwari19191@gmail.com>
2025-03-13 15:57:38 +0530

    learn get,post,delete api

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ const express = require("express")
 
 const app =express()
 
-//request handler functions
-app.use("/test", (req,res)=>{
-    res.send("This is the test url");
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:'sakshi',lastname:'maheshwari'})
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("This is hello url from the server!")
+app.post("/user",(req,res)=>{
+    res.send("Data is saved successfully")
 })
+
+
+app.delete("/user",(req,res)=>{
+    res.send("Data is Deleted  successfully")
+})
+
+
 app.listen(3001,()=>{
     console.log('Server is listening on port 3001...')
 })
